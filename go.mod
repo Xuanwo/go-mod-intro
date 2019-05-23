5480fac527e71149173c937dba85e6010ca5b2d9
Xuanwo <xuanwo@yunify.com>
2019-05-23 17:58:09 +0800

    Let's rock

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Xuanwo/go-mod-intro/v2
+
+go 1.12
+
+require (
+	github.com/google/go-github/v24 v24.0.1
+	github.com/google/go-github/v25 v25.0.4
+)
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	v24 "github.com/google/go-github/v24/github"
+	v25 "github.com/google/go-github/v25/github"
+)
+
+var (
+	_ = v24.Tag{}
+	_ = v25.Tag{}
+)
+
+func main() {
+	return
+}
