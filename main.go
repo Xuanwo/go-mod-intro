24dbaa8cfdf015ac345299d74995ae675f3dea10
Xuanwo <xuanwo@yunify.com>
2019-05-28 12:20:50 +0800

    Add golang.org/x/text/width

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	v24 "github.com/google/go-github/v24/github"
 	v25 "github.com/google/go-github/v25/github"
+	"golang.org/x/text/width"
 )
 
 var (
 	_ = v24.Tag{}
 	_ = v25.Tag{}
+	// Import golang.org/x/text/width to show minimal version select
+	_ = width.EastAsianAmbiguous
+	// Import  github.com/yunify/qscamel/utils to show auto import
+	//_ = utils.Conn{}
 )
 
 func main() {
